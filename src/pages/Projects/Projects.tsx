c4cb673ddf212a061b8fe817c063350b2a51956c
Garrett Sidle <garrett.sidle@gmail.com>
2023-01-13 00:16:05 -0500

    Updated Projects page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,30 +1,24 @@
 import React from "react";
 import { Component } from "react";
 import { ProjectsData } from "./ProjectsData";
+import { FaGithub } from "react-icons/fa";
+import { MdLiveTv } from "react-icons/md";
 
 import "./Projects.css";
+import { Col, Row } from "react-bootstrap";
 
 export class Projects extends Component<{}> {
   private projectHeader() {
     return (
       <div className="Portfolio-Text">
-        <h2 className="title-font pink-text h2-tag">My Portfolio</h2>
-        <p className="white-text p-tag">
+        <h2>
+          My <strong className="Orange">Portfolio</strong>
+        </h2>
+        <div className="Project-Intro">
           I have spent a lot of time on different projects. I love doing
-          everything from scratch. Here are some of my favorite ones which sum
-          up my knowledge. I have done many small projects from different
-          courses and challenges to learn the basics. You can check them on my{" "}
-          <span>
-            <a
-              href="https://github.com/GarrettSidle" 
-              className="Portfolio-Text-Github"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-          </span>
-        </p>
+          everything from scratch. Although I have done many small projects that
+          are not inclueded here.
+        </div>
       </div>
     );
   }
@@ -34,28 +28,92 @@ export class Projects extends Component<{}> {
       <div className="Pojects">
         {ProjectsData.map((project) => (
           <div key={project.id} className="Project">
-            <img
-              src="/Personal-Website.png"
-              alt={project.name}
-              className="Project-Image"
-            ></img>
+            <div className="Project-Title"> {project.name}</div>
+            <div className="Project-Date"> {project.date}</div>
+            {this.projectImages(project)}
             <div className="Project-Description">
-              <p className="white-text p-tag">{project.description}</p>
+              <p className="">{project.description}</p>
               <div className="Project-Meta-Stack ">
                 {project.stack.map((stackName, index) => (
                   <p key={index}>{stackName}</p>
                 ))}
               </div>
-              <div className="Project-Links">
-                <a href={project.source} target="_blank" rel="noreferrer">
-                  <img src={"/project-source.png"} alt="Github icon"></img>
-                </a>
+              <div>
+                <div className="Project-Links">
+                  {project.source !== "" ? this.github(project.source) : ""}
+                  {project.demo !== "" ? this.demo(project.demo) : ""}
+                </div>
               </div>
             </div>
           </div>
         ))}
       </div>
     );
+  }
+
+  private github(source: string) {
+    return (
+      <Col>
+        <a href={source} target="_blank" rel="noreferrer">
+          <Row className="Project-Link">
+            <Col>
+              <FaGithub className="Link-Img" color="white" />
+            </Col>
+            <Col>
+              <div className="Link-Desc">Source</div>
+            </Col>
+          </Row>
+        </a>
+      </Col>
+    );
+  }
+
+  private demo(link: string) {
+    return (
+      <Col>
+        <a href={link} target="_blank" rel="noreferrer">
+          <Row className="Project-Link">
+            <Col>
+              <MdLiveTv className="Link-Img" color="white" />
+            </Col>
+            <Col>
+              <div className="Link-Desc">Demo</div>
+            </Col>
+          </Row>
+        </a>
+      </Col>
+    );
+  }
+
+  private projectImages(project: any) {
+    if (project.secondayImgPath === "") {
+      return (
+        <img
+          src={project.imgPath}
+          alt={project.name}
+          className={"Project-Image-Single"}
+        />
+      );
+    } else {
+      return (
+        <Row>
+          <Col>
+            <img
+              src={project.imgPath}
+              alt={project.name}
+              className="Project-Image-Double Right"
+            />
+          </Col>
+          <Col>
+            <img
+              src={project.secondayImgPath}
+              alt={project.name}
+              className="Project-Image-Double " 
+            />
+          </Col>
+        </Row>
+      );
+    }
   }
 
   public render() {
--- src/pages/Projects/ProjectsData.ts
+++ src/pages/Projects/ProjectsData.ts
@@ -1,20 +1,40 @@
 const ProjectsData = [
-    {
-      id: "personal-website",
-      name: "Personal Website",
-      stack: ["< React.js />", "< CSS3 />", "< Typescript />"],
-      source: "change me",
-      description:
-        "Pizza Time is a pizza restaurant that offers an online menu with various options like pizza, sushi, or pasta. The idea of this project is to make it as close as possible to an actual food e-commerce website which has features like an online menu, order options, and so on.",
-    },
-    {
-      id: "crud-web-application",
-      name: "CRUD Web Application",
-      stack: ["< RESTful API />", "< CSS3 />", "< React.js />", "< Nest.js />", , "< Typescript />",, "< MySQL />"],
-      source: "change me",
-      description:
-        "REST Countries is a project made with API which also has a color theme switcher feature. You can view the list of all countries and find a specific one via search or by filtering regions. By navigating to the individual country page, you are able to see more details about the country like native name and even border countries.",
-    }
-  ];
+  {
+    id: "personal-website",
+    name: "Personal Website",
+    stack: ["< React.js />", "< CSS3 />", "< Typescript />"],
+    source: "https://github.com/GarrettSidle/Personal-Website",
+    date:"June 2022 - Present",
+    demo:"/Home",
+    imgPath:"/Personal-Website.png",
+    secondayImgPath:"",
+    description:
+      "This project is the website you are currently on, built using the React framework. The website was designed from scratch utilizing the React-Bootstrap library. It was deployed to AWS for hosting. The website serves as an online portfolio and personal branding platform for me. It showcases my skills, experiences and other projects of mine. It was later updated to communicate with an API backend to allow for messages to be sent through the website.",
+  },
+  {
+    id: "crud-web-application",
+    name: "CRUD Web Application",
+    stack: ["< RESTful API />", "< CSS3 />", "< React.js />", "< Nest.js />", , "< Typescript />", "< MySQL />"],
+    source: "",
+    date:"December 2022",
+    demo:"",
+    imgPath:"",
+    secondayImgPath:"",
+    description:
+      "This project is a CRUD (Create, Read, Update, and Delete) web application that allows users to interact with a database through a front-end interface. The backend of the application is built using the Nest.js framework, which is a powerful and flexible JavaScript framework for building efficient and scalable server-side applications. The database used in this project is MySQL, a popular open-source relational database management system. The application allows users to create new records, view existing records, update existing records, and delete records from the database. The communication between the front-end and the backend is done through RESTful API calls, which allow the webpage to securely and efficiently access the database and make the necessary changes. REST(Representational State Transfer) is a standard architectural style for building web services, these API calls are based on the REST principles and can be easily consumed by any web-based client.",
+  },
+  {
+    id: "rc-car",
+    name: "Remote-Control Car",
+    stack: ["< C++ />", "< Electronic Design />", "< Ardiuno Programming />"],
+    source: "https://github.com/GarrettSidle/RC-Car",
+    date:"October 2022",
+    demo:"",
+    imgPath:"/RC-Car1.png",
+    secondayImgPath:"/RC-Car2.png",
+    description:
+      "This project involves the construction of a remote-controlled car using an Arduino microcontroller and DC motors. The car is controlled via a wireless communication module, allowing for remote operation. C++ programming language is used to write the code for the Arduino, which is responsible for controlling the motors and receiving commands from the remote controller. The car's speed and direction can be adjusted using the remote, and it also includes features such as obstacle detection and automatic braking. The project demonstrates the integration of hardware and software to create a functional and user-friendly device.",
+  }
+];
 
-  export { ProjectsData };
+export { ProjectsData };
