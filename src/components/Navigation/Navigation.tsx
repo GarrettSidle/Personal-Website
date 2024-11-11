5c7ad835db6e72c5aa137d6d287e6165a0e20ee5
unknown <garrett.sidle@gmail.com>
2024-11-11 02:28:19 -0500

    redid nav with no bootstrap

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,50 +1,44 @@
-import React from "react";
 import { Component } from "react";
-import { Nav, Navbar} from "react-bootstrap";
 
 import "./Navigation.css";
 
 
+export class Navigation extends Component<{},{ navUnfolded: boolean }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      navUnfolded: false, 
+    };
+  }
 
-export class Navigation extends Component<{}> {
+  toggleNav = () => {
+    this.setState((prevState) => ({
+      navUnfolded: !prevState.navUnfolded, 
+    }));
+  };
+
 
   public render() {
     return (
-      <div>
-        <Navbar expand="lg" className="Navigation fixed-top" >
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse>
-              <Navbar.Brand className="Nav-Brand" href="/#Home">Garrett Sidle</Navbar.Brand>
-              <Nav className="Nav-Links">
-                <Nav.Item>
-                  <Nav.Link className="Nav-Link" href="/#Home">HOME</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link className="Nav-Link" href="/#Education">EDUCATION</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link className="Nav-Link" href="/#Projects">PROJECTS</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link className="Nav-Link" href="/#About">ABOUT</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link className="Nav-Link" href="/#Contact">CONNECT</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link
-                  className="Nav-Link" 
-                    href="https://github.com/GarrettSidle"
-                    target="blank"
-                  >
-                    GITHUB
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <div className={this.state.navUnfolded?"Enabled":""}>
+        <nav className={`Navigation fixed-top`}>
+          <div className="Nav-Objects">
+            <a href="/#Home" className="Nav-Brand">Garrett Sidle</a>
+            <button className="Hamburger" onClick={()=>{this.toggleNav()}}>☰</button>
+            <div className="Nav-Links">
+              <a className="Nav-Link" href="/#Home">HOME</a>
+              <a className="Nav-Link" href="/#Education">EDUCATION</a>
+              <a className="Nav-Link" href="/#Projects">PROJECTS</a>
+              <a className="Nav-Link" href="/#About">ABOUT</a>
+              <a className="Nav-Link" href="/#Contact">CONNECT</a>
+              <a className="Nav-Link" href="https://github.com/GarrettSidle" target="_blank">GITHUB</a>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
+
+
 }
 export default Navigation;
