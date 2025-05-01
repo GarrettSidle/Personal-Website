a16c71310cae4f4ad0374b7ee538060fbcabe9f1
unknown <garrett.sidle@gmail.com>
2025-05-01 17:33:37 -0400

    added GATech

--- src/pages/Education/Education.tsx
+++ src/pages/Education/Education.tsx
@@ -6,11 +6,12 @@ import Header from "../../components/Header/Header";
 export class Education extends Component<{}> {
   private card(
     header: string,
+    underHeader: String,
     subHeader: string,
     dates: string,
     link: string,
     img: any,
-    className:string
+    className: string
   ) {
     return (
       <div className={`Card ${className}`}>
@@ -19,7 +20,8 @@ export class Education extends Component<{}> {
             <img className="Card-Logo" src={img} />
           </div>
           <div className="Card-Header">{header}</div>
-          <hr className="Edu-HR"/>
+          <div className="Card-Underheader">{underHeader}</div>
+          <hr className="Edu-HR" />
           <div className="Card-SubHeader">{subHeader}</div>
 
           <div className="Card-Dates">{dates}</div>
@@ -34,35 +36,47 @@ export class Education extends Component<{}> {
         <Header>My Education</Header>
         <div className="Education">
           {this.card(
-              "Bachelor's: Electrical and Computer Engineering",
-              "Miami University of Ohio",
-              "Aug 2023 – May 2025",
-              "https://programs.miamioh.edu/programs/electrical-computer-engineering-technology-bs/",
-              "/assets/Miami.png",
-              "Miami"
-            )}
-        <div className="Cards">
-          <div className="col">
-            {this.card(
-              "Associate's: Electro-Mechanical Engineering",
-              "Northwest State Community College",
-              "Aug 2021 – Nov 2023",
-              "https://northweststate.edu/electro-mechanical-engineering-technology",
-              "/assets/Northwest.png",
-              "Northwest"
-            )}
+            "Masters's: Computer Science",
+            "Specialization: Computing Systems",
+            "Georgia Institute of Technology",
+            "Aug 2025 - Nov 2027",
+            "https://omscs.gatech.edu/specialization-computing-systems",
+            "/assets/GATech.png",
+            "GATech"
+          )}
+          {this.card(
+            "Bachelor's: Electrical and Computer Engineering",
+            "",
+            "Miami University of Ohio",
+            "Aug 2023 – May 2025",
+            "https://programs.miamioh.edu/programs/electrical-computer-engineering-technology-bs/",
+            "/assets/Miami.png",
+            "Miami"
+          )}
+          <div className="Cards">
+            <div className="col">
+              {this.card(
+                "Associate's: Electro-Mechanical Engineering",
+                "",
+                "Northwest State Community College",
+                "Aug 2021 – Nov 2023",
+                "https://northweststate.edu/electro-mechanical-engineering-technology",
+                "/assets/Northwest.png",
+                "Northwest"
+              )}
+            </div>
+            <div className="col">
+              {this.card(
+                "Associate's: Computer Science Engineering",
+                "",
+                "Northwest State Community College",
+                "Aug 2021 – May 2023",
+                "https://northweststate.edu/computer-science-engineering-technology",
+                "/assets/Northwest.png",
+                "Northwest"
+              )}
+            </div>
           </div>
-          <div className="col">
-            {this.card(
-            "Associate's: Computer Science Engineering",
-              "Northwest State Community College",
-              "Aug 2021 – May 2023",
-              "https://northweststate.edu/computer-science-engineering-technology",
-              "/assets/Northwest.png",
-              "Northwest"
-            )}
-          </div>
-        </div>
         </div>
       </div>
     );
