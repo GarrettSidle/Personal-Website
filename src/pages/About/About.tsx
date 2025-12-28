fe480809c0d4db5319be6d19df129505651e8f2d
Garrett Sidle <garrett.sidle@gmail.com>
2025-12-28 17:18:48 -0500

    update about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,39 +1,61 @@
 import { Component } from "react";
 import { CgCPlusPlus } from "react-icons/cg";
 import { AiOutlineConsoleSql, AiOutlineCode } from "react-icons/ai";
-import { DiReact, DiPython, DiGit, DiJava } from "react-icons/di";
-import { SiLinux, SiTypescript, SiCsharp, SiTensorflow, SiTestcafe } from "react-icons/si";
+import { DiReact, DiPython, DiGit, DiJava, DiPostgresql } from "react-icons/di";
+import {
+  SiLinux,
+  SiTypescript,
+  SiCsharp,
+  SiTensorflow,
+  SiTestcafe,
+  SiFastapi,
+  SiMicrosoftazure,
+  SiDocker,
+  SiRedis,
+  SiDotnet,
+  SiGradle,
+  SiMicrosoftsqlserver,
+  SiApachekafka,
+  SiAzuredevops,
+  SiKubernetes,
+  SiGrafana,
+  SiSpring,
+} from "react-icons/si";
 import { FaAws, FaNodeJs } from "react-icons/fa";
-import { GrVmware, GrGraphQl } from "react-icons/gr";
+import { GrVmware } from "react-icons/gr";
 import { IoLogoTableau } from "react-icons/io5";
 import { MdOutlineCopyright } from "react-icons/md";
-
 
 import "./About.css";
 
 const techStack = [
-  { Component: SiCsharp, Tooltip: "C#" },
-  { Component: SiTypescript, Tooltip: "TypeScript" },
-  { Component: DiReact, Tooltip: "React" },
-  { Component: DiPython, Tooltip: "Python" },
-  { Component: AiOutlineConsoleSql, Tooltip: "SQL" },
-  { Component: AiOutlineCode, Tooltip: "MATLAB" },
-  { Component: CgCPlusPlus, Tooltip: "C++" },
   { Component: DiJava, Tooltip: "Java" },
+  { Component: SiSpring, Tooltip: "Spring Boot" },
+  { Component: SiCsharp, Tooltip: "C#" },
+  { Component: SiDotnet, Tooltip: ".NET 6+" },
+  { Component: SiMicrosoftsqlserver, Tooltip: "SQL Server" },
+  { Component: DiPostgresql, Tooltip: "PostgreSQL" },
+  { Component: SiApachekafka, Tooltip: "Apache Kafka" },
+  { Component: AiOutlineConsoleSql, Tooltip: "SQL" },
+  { Component: DiPython, Tooltip: "Python" },
+  { Component: SiFastapi, Tooltip: "FastAPI" },
   { Component: FaNodeJs, Tooltip: "Node.js" },
-  { Component: GrGraphQl, Tooltip: "GraphQL" },
-  { Component: MdOutlineCopyright, Tooltip: "C" },
-  { Component: SiTensorflow, Tooltip: "Tensorflow" },
+  { Component: SiTypescript, Tooltip: "TypeScript" },
 ];
 
 const toolStack = [
+  { Component: SiMicrosoftazure, Tooltip: "Azure Cloud" },
+  { Component: FaAws, Tooltip: "AWS" },
+  { Component: SiKubernetes, Tooltip: "Kubernetes" },
+  { Component: SiAzuredevops, Tooltip: "Azure DevOps CI/CD" },
+  { Component: SiDocker, Tooltip: "Docker" },
   { Component: DiGit, Tooltip: "Git" },
   { Component: SiLinux, Tooltip: "Linux" },
+  { Component: SiGrafana, Tooltip: "Grafana" },
+  { Component: SiRedis, Tooltip: "Redis" },
+  { Component: SiGradle, Tooltip: "Maven / Gradle" },
+  { Component: SiMicrosoftazure, Tooltip: "Azure Service Bus" },
   { Component: GrVmware, Tooltip: "VMWare" },
-  { Component: FaAws, Tooltip: "AWS" },
-  { Component: IoLogoTableau, Tooltip: "Tableau" },
-  { Component: SiTestcafe, Tooltip: "TestCafe" },
-
 ];
 
 export class About extends Component<{}> {
@@ -65,28 +87,30 @@ export class About extends Component<{}> {
 
   public render() {
     return (
-      <div className="About-Section Page" id="About" >
-
-
+      <div className="About-Section Page" id="About">
         <div className="About-Me">
           <div className="About-Paragraph">
-            <h1 className="About-Heading" >
+            <h1 className="About-Heading">
               More About <strong className="Orange">Me</strong>
             </h1>
-            I'm a dedicated software engineer with hands-on experience from internships at Automatic
-            Feed Company, Leonardo DRS, and GEICO. My background is rooted in both low-level and backend
-            systems, where I’ve applied skills in C#, Python, C++, and MATLAB to build efficient, scalable
-            solutions. I thrive on tackling complex challenges and developing impactful applications that
-            bridge technology with practical solutions. I’m always looking to expand my knowledge and contribute
-            to innovative projects.
+            I'm a backend engineer with experience building enterprise-grade
+            services and APIs. I work on backend systems that power critical
+            business operations. My work centers around maximizing uptime for
+            our services abd developing new features, ensuring they remain
+            reliable and available for users. I focus on monitoring system
+            health, identifying potential issues before they impact users, and
+            implementing improvements that enhance stability and performance.
+            I'm continuously learning and growing in my role, taking on new
+            challenges. When I'm not coding, I enjoy working out to stay active,
+            playing chess, exploring new technologies and side projects in my
+            spare time.
           </div>
           <img
-            src="/assets/Headshot.jpg"
+            src="/assets/Headshot.png"
             alt="about"
             className="About-Headshot"
           />
         </div>
-
 
         <h1 className="Stack-Heading">
           Professional <strong className="Orange">Skillset </strong>
@@ -97,7 +121,6 @@ export class About extends Component<{}> {
           <strong className="Orange">Tools</strong> I use
         </h1>
         {this.ToolStack()}
-
       </div>
     );
   }
